4f7dbc72f42d21011ed011e02fa173d8e20be4d8
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-07 16:48:06 -0600

    Proyectos que se usaron para hacer la practica de ASP.NET Core for beginners

---
+++ EjercicioFirstCSharpCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the first line.");
+            Console.WriteLine("This is the second line.");
+            
+            Console.WriteLine("This is the first line.");
+
+            Console.Write("This is ");
+            Console.Write("the second ");
+            Console.Write("line.");
+        }
+    }
+}
